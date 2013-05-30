77f35cc4b35a7664ed240f4637b31767961da468
Tabea Treutwein <tabea.treutwein@gmx.de>
2013-05-30 20:49:17 +0200

    change of upload

--- src/main/java/de/phoenix/consoleclient/core/Core.java
+++ src/main/java/de/phoenix/consoleclient/core/Core.java
@@ -20,16 +20,14 @@ package de.phoenix.consoleclient.core;
 
 import java.io.File;
 
-import javax.ws.rs.core.MediaType;
-
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
-import com.sun.jersey.multipart.FormDataMultiPart;
-import com.sun.jersey.multipart.file.FileDataBodyPart;
 import com.sun.jersey.multipart.impl.MultiPartWriter;
+
+import de.phoenix.util.UploadHelper;
 
 public class Core {
 
@@ -54,27 +52,30 @@ public class Core {
 
         // current directory + filename
         File file = new File(path);
-        String author = System.getProperty("user.name");
+        // String author = System.getProperty("user.name");
 
         if (!file.exists()) {
             System.out.println("File doesn't exist.");
             return;
         }
 
-        ClientConfig cc = new DefaultClientConfig();
-        cc.getClasses().add(MultiPartWriter.class);
-        Client client = Client.create(cc);
-        WebResource wr = client.resource("http://meldanor.dyndns.org:8080/PhoenixWebService/rest").path("/submission").path("/upload").path(author);
-
+        // ClientConfig cc = new DefaultClientConfig();
+        // cc.getClasses().add(MultiPartWriter.class);
+        Client client = Client.create();
+        //WebResource wr = client.resource("http://meldanor.dyndns.org:8080/PhoenixWebService/rest").path("/submission").path("/upload").path(author);
+        WebResource resource = client.resource("http://meldanor.dyndns.org:8080/PhoenixWebService/rest").path("/submission").path("/submit");
+        
         // create file packet
-        FormDataMultiPart multiPart = new FormDataMultiPart();
-        if (file != null) {
-            multiPart.bodyPart(new FileDataBodyPart("file", file, MediaType.APPLICATION_OCTET_STREAM_TYPE));
-        }
+        //FormDataMultiPart multiPart = new FormDataMultiPart();
+        //if (file != null) {
+          //  multiPart.bodyPart(new FileDataBodyPart("file", file, MediaType.APPLICATION_OCTET_STREAM_TYPE));
+        //}
 
         // Send file to server
-        ClientResponse cr = wr.type(MediaType.MULTIPART_FORM_DATA_TYPE).put(ClientResponse.class, multiPart);
-        System.out.println("Response: " + cr.getClientResponseStatus());
+        ClientResponse response = UploadHelper.uploadFile(resource, file);
+        System.out.println("Hallo hier" + response);
+        // ClientResponse cr = wr.type(MediaType.MULTIPART_FORM_DATA_TYPE).put(ClientResponse.class, multiPart);
+        System.out.println("Respons: " + response.getClientResponseStatus());
     }
 
     public static void main(String[] args) {
@@ -82,13 +83,13 @@ public class Core {
         ClientConfig cc = new DefaultClientConfig();
         cc.getClasses().add(MultiPartWriter.class);
         Client client = Client.create(cc);
-       
+
         WebResource wr = client.resource("http://meldanor.dyndns.org:8080/PhoenixWebService/rest/helloworld");
 
         System.out.println(wr.get(String.class));
 
-        if(args.length > 0){
-        // "java -jar ... upload file"
+        if (args.length > 0) {
+            // "java -jar ... upload file"
             if (args[0].toLowerCase().equals("upload")) {
                 upload(args[1]);
                 // "java -jar ... download file"
@@ -101,7 +102,6 @@ public class Core {
             System.out.println("[USAGE]: java -jar ... {upload, download} filepath");
             return;
         }
-        
 
     }
 
