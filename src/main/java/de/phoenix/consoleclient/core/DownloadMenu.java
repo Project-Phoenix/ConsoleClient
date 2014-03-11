f4d621385944f2c703fd26b0a2a7ed81d73e0b54
Tabea Treutwein <tabea.treutwein@gmx.de>
2014-03-11 16:42:59 +0100

    change intern download structure and add attachments

--- src/main/java/de/phoenix/consoleclient/core/DownloadMenu.java
+++ src/main/java/de/phoenix/consoleclient/core/DownloadMenu.java
@@ -20,6 +20,7 @@ package de.phoenix.consoleclient.core;
 
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
@@ -33,6 +34,7 @@ import com.sun.jersey.api.client.WebResource;
 import de.phoenix.filter.EduFilter;
 import de.phoenix.filter.TextFilter;
 import de.phoenix.rs.EntityUtil;
+import de.phoenix.rs.entity.PhoenixAttachment;
 import de.phoenix.rs.entity.PhoenixTask;
 import de.phoenix.rs.entity.PhoenixTaskSheet;
 import de.phoenix.rs.entity.PhoenixText;
@@ -63,6 +65,20 @@ public class DownloadMenu extends Menu {
         return path;
     }
 
+    public void writeInFile(File file, String text) {
+        Writer fw;
+        Writer bw;
+        try {
+            fw = new FileWriter(file);
+            bw = new BufferedWriter(fw);
+            bw.write(text);
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     public void execute(String[] args) throws Exception {
 
         // storage location
@@ -87,47 +103,87 @@ public class DownloadMenu extends Menu {
 
         PhoenixTask reqTitle = EntityUtil.extractEntity(post);
         List<PhoenixText> pattern = reqTitle.getPattern();
+        List<PhoenixAttachment> attachment = reqTitle.getAttachments();
 
-        File dir = new File(path);
-        File file = new File(dir, taskTitle + ".java");
-        if (file.exists()) {
-            System.out.println("File already exists.");
-            return;
-        } else {
-            try {
-                file.createNewFile();
-            } catch (IOException e) {
-                System.out.println("File couldn't be created.");
-                e.printStackTrace();
-            }
-            System.out.println("BUILT!");
-        }
+        // TODO: Datei erstellen, wos hingeschrieben wird
+        // TODO: bei mehreren pattern in Ordner packen
+        // TODO: description und pattern in Datei schreiben
 
-        // TODO: attachements? bilder?
         TextFilter t = EduFilter.INSTANCE;
         String description = reqTitle.getDescription();
+        if (description.equals("")) {
+            System.out.println("Description is empty.");
+            return;
+        }
         String descrFiltered = t.filter(description);
-        Writer fw = new FileWriter(file);
-        Writer bw = new BufferedWriter(fw);
 
+        File directory = new File(path);
 
+        // task is without any given code or attachment, just text
+        if (pattern.isEmpty() && attachment.isEmpty()) {
 
-        if (pattern.isEmpty()) {
-            bw.write(descrFiltered);
-            bw.close();
-        }
+            File file = new File(directory, taskTitle + ".java");
 
-        // eine Klasse ein Text
-        for (PhoenixText hans : pattern) {
-            try {
-                bw.write(hans.getText() + "\n");
-                bw.write("/*" + descrFiltered + "*/");
-                bw.close();
-            } catch (IOException e) {
-                e.printStackTrace();
+            if (file.exists()) {
+                System.out.println("File already exists.");
+                return;
             }
-        }
 
+            // if file doesn't exist, create this file and write the
+            // description as a comment in it
+            writeInFile(file, "/*" + descrFiltered + "*/");
+
+            // task has one pattern hence one class to submit, no attachments
+        } else if (pattern.size() == 1 && attachment.isEmpty()) {
+
+            File file = new File(directory, pattern.get(0).getFullname());
+
+            if (file.exists()) {
+                System.out.println("File already exists.");
+                return;
+            }
+            writeInFile(file, "/*" + descrFiltered + "*/" + pattern.get(0).getText());
+
+            // task has at least two patterns or some attachments
+        } else {
+
+            File dir = new File(directory, taskTitle);
+            if (dir.exists()) {
+                System.out.println("Directory already exists.");
+                return;
+            }
+            dir.mkdir();
+
+            if (!pattern.isEmpty()) {
+                // writes each class in a file in the directory
+                for (PhoenixText clazz : pattern) {
+
+                    File file = new File(directory + "/" + taskTitle, clazz.getFullname());
+                    writeInFile(file, "/*" + description + "*/\n" + clazz.getText());
+
+                }
+            }
+            
+            if(!attachment.isEmpty()) {
+                
+                if(pattern.isEmpty()) {
+                    File file = new File(directory + "/" + taskTitle, taskTitle + ".java");
+                    writeInFile(file, descrFiltered);
+                }
+                    
+                for(PhoenixAttachment picture : attachment) {
+                    
+                    File file = new File(directory + "/" + taskTitle, picture.getFullname());
+                    byte[] content = picture.getContent();
+                    FileOutputStream fos = new FileOutputStream(file);
+                    fos.write(content);
+                    fos.close();
+                }
+            }
+
+        }
     }
+
+
 
 }
