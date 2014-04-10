07e7203aa0668cbce55b94b66bde775a3a9e6755
Tabea Treutwein <tabea.treutwein@gmx.de>
2014-04-10 22:01:25 +0200

    next try

---
+++ src/main/java/de/phoenix/consoleclient/core/UploadHandler.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2014 Project-Phoenix
+ * 
+ * This file is part of ConsoleClient.
+ * 
+ * ConsoleClient is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * ConsoleClient is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with ConsoleClient.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.phoenix.consoleclient.core;
+
+public class UploadHandler {
+    
+    private Upload upload;
+    
+    public UploadHandler(String[] args) {
+        upload = new Upload();
+    }
+    
+    public void execute(String[] args) throws Exception {
+        upload.execute(args);
+    }
+
+}
